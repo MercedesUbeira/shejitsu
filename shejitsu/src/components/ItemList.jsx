2affb46e9e2fc3107ca0c50f46919253e1f69513
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2024-11-08 14:47:10 +0100

    Added a comment

--- shejitsu/src/components/ItemList.jsx
+++ shejitsu/src/components/ItemList.jsx
@@ -16,3 +16,8 @@ export default function ItemList({ items, type }) {
     </div>
   );
 }
+
+// A comment for us in the future.
+//The ItemList component shows a list of items that can either be "Courses" or "Techniques," depending on what you pass in as the type prop. 
+//It loops through the items array and, if the type is "courses," it uses a CourseCard component to display each item; otherwise, it uses a TechniqueCard component. 
+//This way, the component can switch between showing courses or techniques based on what’s needed.
